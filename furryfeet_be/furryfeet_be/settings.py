66e34b0ffb39c41ef24cea9fe5044e3ae69d5567
SaraMisajlovska <smisajlovska@gmail.com>
2023-04-16 21:50:02 +0200

    remove secret key

--- furryfeet_be/furryfeet_be/settings.py
+++ furryfeet_be/furryfeet_be/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0$xfno-gb*tz=8(b+_3ce#jsq^zzt1t^31a0*3a@pax=)oy5-f'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
