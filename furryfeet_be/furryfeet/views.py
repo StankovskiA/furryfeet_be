cf43652e41b9ed0533962d60764de2b3e848f136
StankovskiA <stankovskia@gmail.com>
2023-04-17 17:19:11 +0200

    UIK-24: Hello World endpoints

--- furryfeet_be/furryfeet/views.py
+++ furryfeet_be/furryfeet/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import MyModelSerializer
+from rest_framework.views import APIView
+from rest_framework import generics
+from .models import MyModel
 
 # Create your views here.
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"message": "Hello World!"})
+    
+class MyModelList(generics.ListCreateAPIView):
+    queryset = MyModel.objects.all()
+    serializer_class = MyModelSerializer
