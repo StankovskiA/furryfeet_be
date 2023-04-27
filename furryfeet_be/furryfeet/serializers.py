6c248dd90af7f4399006e0757879cc363c6ed0ff
StankovskiA <stankovskia@gmail.com>
2023-04-27 14:58:55 +0200

    UIK-31: Create User Model

--- furryfeet_be/furryfeet/serializers.py
+++ furryfeet_be/furryfeet/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
-from .models import MyModel
+from .models import MyModel, User
 
 class MyModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyModel
         fields = '__all__'
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}    
+        }
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        
+        return instance
+            
--- furryfeet_be/furryfeet/urls.py
+++ furryfeet_be/furryfeet/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import HelloWorld, MyModelList
+from .views import HelloWorld, LoginView, LogoutView, MyModelList, RegisterView, UserView
 
 urlpatterns = [
     path('hello/', HelloWorld.as_view()),
     path('mymodels/', MyModelList.as_view()),
+    
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('user/', UserView.as_view()),
 ]
