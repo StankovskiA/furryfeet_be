f59e77047d5fc0df9aea3f38507ce929971505a8
Murat <muratuseini@gmail.com>
2023-04-30 15:59:38 +0200

    added URL patterns

--- furryfeet_be/furryfeet/urls.py
+++ furryfeet_be/furryfeet/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HelloWorld, LoginView, LogoutView, MyModelList, RegisterView, UserView, AddUserImageView
+from .views import HelloWorld, LoginView, LogoutView, MyModelList, RegisterView, UserView, AddUserImageView,DogWalkerView,AppointmentView,FeedbackView
 
 urlpatterns = [
     path('hello/', HelloWorld.as_view()),
@@ -10,4 +10,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path('user/', UserView.as_view()),
     path('user/<int:pk>/image/', AddUserImageView.as_view()),
+    path('dog-walker/', DogWalkerView.as_view(), name='dog-walker-get'),  # GET request to retrieve dog walker info, appointments and feedbacks
+    path('dog-walker/<int:pk>/', DogWalkerView.as_view(), name='dog-walker-update'),  # POST request to update dog walker info
+    path('appointments/', AppointmentView.as_view(), name='appointment-get'),  # GET request to retrieve appointments
+    path('appointments/<int:pk>/', AppointmentView.as_view(), name='appointment-create'),  # POST request to create a new appointment
+    path('feedbacks/', FeedbackView.as_view(), name='feedback-get'),  # GET request to retrieve feedbacks
+    path('feedbacks/<int:pk>/', FeedbackView.as_view(), name='feedback-create'),  # POST request to create a new feedback
 ]
