713e2fec07d97b8b45a2fd17d25671f50049c2ae
VladimirIvanovski <vladimir.ivanovski5@gmail.com>
2023-05-01 02:12:02 +0200

    Made the models for the dog details and the feedback model.py

--- furryfeet_be/furryfeet/admin.py
+++ furryfeet_be/furryfeet/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import MyModel
+from .models import MyModel, User, DogDetails, Feedback
 
 # Register your models here.
 admin.site.register(MyModel)
+admin.site.register(User)
+admin.site.register(DogDetails)
+admin.site.register(Feedback)
--- furryfeet_be/furryfeet/models.py
+++ furryfeet_be/furryfeet/models.py
@@ -8,7 +8,18 @@ class MyModel(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class DogDetails(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    age = models.IntegerField()
+    dog_id = models.CharField(max_length=50)
+    feedbacks = models.TextField()
+    dog_photo = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -16,7 +27,23 @@ class User(AbstractUser):
     password = models.CharField(max_length=20)
     joined_at = models.DateTimeField(auto_now_add=True)
     image = models.URLField(null=True, blank=True)
-    username = None
+    username = models.CharField(max_length=20,blank=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username',]
+    dog_details = models.ForeignKey(DogDetails, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Feedback(models.Model):
+    RATING_CHOICES = (
+        (1, '1 - Poor'),
+        (2, '2 - Fair'),
+        (3, '3 - Average'),
+        (4, '4 - Good'),
+        (5, '5 - Excellent')
+    )
+
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    sender = models.ForeignKey(User, related_name='sent_feedback', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_feedback', on_delete=models.CASCADE)
