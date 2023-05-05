ccdf9a4c7b9c581e879f6e2ed91b2ec68bc18125
SaraMisajlovska <smisajlovska@gmail.com>
2023-05-05 19:54:54 +0200

    UIK-31-Created initial models and added tests

--- furryfeet_be/furryfeet/models.py
+++ furryfeet_be/furryfeet/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+RATING_CHOICES = (
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+)
+
+
 # Create your models here.
 class MyModel(models.Model):
     name = models.CharField(max_length=50)
@@ -8,7 +17,8 @@ class MyModel(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -16,7 +26,56 @@ class User(AbstractUser):
     password = models.CharField(max_length=20)
     joined_at = models.DateTimeField(auto_now_add=True)
     image = models.URLField(null=True, blank=True)
+    is_dog_walker = models.BooleanField(default=False)
     username = None
-    
-    USERNAME_FIELD = 'email'
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class Feedback(models.Model):
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    user_from = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="feedback_given",
+        limit_choices_to={"is_dog_walker": False},
+    )
+    user_to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="feedback_received",
+        limit_choices_to={"is_dog_walker": True},
+    )
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    age = models.IntegerField()
+    tag = models.CharField(max_length=50, unique=True)
+    photo = models.URLField(null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="dogs_owned")
+
+
+class DogFeedback(models.Model):
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    dog_walker = models.ForeignKey(
+        User, on_delete=models.CASCADE, limit_choices_to={"is_dog_walker": True}
+    )
+    dog = models.ForeignKey(
+        Dog, on_delete=models.CASCADE, related_name="feedback_received"
+    )
+
+
+class Appointment(models.Model):
+    date = models.DateTimeField()
+    dog_walker = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="appointments_owned",
+        limit_choices_to={"is_dog_walker": True},
+    )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
--- furryfeet_be/furryfeet/tests.py
+++ furryfeet_be/furryfeet/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+from datetime import datetime
+from .models import MyModel, User, Feedback, Dog, DogFeedback, Appointment
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(
+            first_name="John", last_name="Doe", email="johndoe@example.com", password="123456", is_dog_walker=True
+        )
+        self.user2 = User.objects.create(
+            first_name="Jane", last_name="Doe", email="janedoe@example.com", password="123456"
+        )
+        self.dog = Dog.objects.create(name="Fido", breed="Labrador", age=3, tag="12345", owner=self.user2)
+        self.feedback = Feedback.objects.create(rating=5, comment="Great dog walker!", user_from=self.user2, user_to=self.user1)
+        self.dog_feedback = DogFeedback.objects.create(rating=4, comment="Fido was a bit stubborn today", dog_walker=self.user1, dog=self.dog)
+        self.appointment = Appointment.objects.create(date=datetime.now(), dog_walker=self.user1, dog=self.dog)
+
+    def test_my_model(self):
+        my_model = MyModel.objects.create(name="Test Model", description="This is a test model")
+        self.assertEqual(str(my_model), "Test Model")
+
+    def test_user_creation(self):
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(first_name="John").email, "johndoe@example.com")
+
+    def test_feedback_creation(self):
+        self.assertEqual(Feedback.objects.count(), 1)
+        self.assertEqual(Feedback.objects.get(user_from=self.user2).user_to, self.user1)
+
+    def test_dog_creation(self):
+        self.assertEqual(Dog.objects.count(), 1)
+        self.assertEqual(Dog.objects.get(name="Fido").owner, self.user2)
+
+    def test_dog_feedback_creation(self):
+        self.assertEqual(DogFeedback.objects.count(), 1)
+        self.assertEqual(DogFeedback.objects.get(dog=self.dog).dog_walker, self.user1)
+
+    def test_appointment_creation(self):
+        self.assertEqual(Appointment.objects.count(), 1)
+        self.assertEqual(Appointment.objects.get(dog=self.dog).dog_walker, self.user1)
